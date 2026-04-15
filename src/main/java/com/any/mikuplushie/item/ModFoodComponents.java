912c9a70a7820b56a872867ad93ff97044d67682
YupCore <k.gavrish07@gmail.com>
2026-04-16 01:17:49 +0300

    moved assets

--- src/main/java/com/any/mikuplushie/item/ModFoodComponents.java
+++
@@ -1,9 +0,0 @@
-package com.any.mikuplushie.item;
-
-import net.minecraft.component.type.FoodComponent;
-import net.minecraft.component.type.FoodComponents;
-
-public class ModFoodComponents extends FoodComponents {
-    public static final FoodComponent BAGUETTE = new FoodComponent.Builder().nutrition(8).saturationModifier(0.6F).build();
-    public static final FoodComponent LEEK = new FoodComponent.Builder().nutrition(4).saturationModifier(0.6F).build();
-}
